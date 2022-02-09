cba62e4a891ca7312b6eabc4c5676cd43c1f1bbd
Emily Bennett <emily.bennett@northcoders.com>
2022-02-09 15:42:05 +0000

    corrected api/recipes test to check nested objects in array

--- test/api.test.js
+++ test/api.test.js
@@ -3,6 +3,7 @@ const server = require('../server');
 
 const request = supertest(server);
 
+
 describe('Lizzos juice bar', () => {
   describe('/api', () => {
     test('/api', async () => {
@@ -16,21 +17,21 @@ describe('Lizzos juice bar', () => {
 
     test('should return an array of all recipes if requeseted with no query term', async () => {
 
-      const {body} = await request.get('/api').expect(200)
+      const {body} = await request.get('/api/recipes').expect(200)
 
-      body.forEach((recipe)=>{
+      body.recipes.forEach((recipe)=>{
 
-        expect(recipe.ingredients).toEqual(expect.objectContaining({ 
+        expect(recipe).toEqual(expect.objectContaining({ 
           "id": expect.any(String),
         "imageUrl": expect.any(String),
         "instructions": expect.any(String),
         "ingredients": expect.any(Object)
         }))
 
-        expect(recipe.ingredients).toEqual(expect.objectContaining({
-          'name': expect.any(String),
+        expect(recipe.ingredients).toEqual(expect.arrayContaining([expect.objectContaining({
+          'name' : expect.any(String),
           'grams' : expect.any(Number)
-        }))
+        })]))
       })
     });
     
