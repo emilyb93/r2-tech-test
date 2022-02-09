e7c808bd8e1025adc9870133ef962e7f1a6a79e9
Emily Bennett <emily.bennett@northcoders.com>
2022-02-09 15:21:26 +0000

    created new routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const apiRouter = require('express').Router();
+const recipeRouter = require('./recipeRouter.js')
 
 apiRouter.get('/', (_, res) => {
   res.json({ message: 'ok' });
 });
+
+apiRouter.use('/recipes', recipeRouter)
 
 module.exports = apiRouter;
---
+++ routes/recipeRouter.js
@@ -0,0 +1,6 @@
+const recipeRouter = require('express').Router();
+
+const {requestRecipes} = require('../controllers/recipe.controller.js')
+
+
+recipeRouter.get('/', requestRecipes)
