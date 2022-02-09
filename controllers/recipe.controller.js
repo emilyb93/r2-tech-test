7c02359a6843cdac48868f23960040c1017c0dc7
Emily Bennett <emily.bennett@northcoders.com>
2022-02-09 15:42:34 +0000

    completed /api/recipe function

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -1,7 +1,11 @@
-const {requestRecipes} = require('../models/recipe.model.js')
+const {fetchRecipes} = require('../models/recipe.model.js')
 
 
 exports.requestRecipes = async (req,res,next)=>{
-   const recipes = await fetchRecipes()
-   console.log(recipes)
+    const recipes =  await fetchRecipes()
+    console.log(Array.isArray(recipes))
+    
+    
+    res.status(200).send({recipes})
+    
 }
